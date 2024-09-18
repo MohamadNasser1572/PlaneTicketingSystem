c4e2ce311c7884d2842b4df98da296517134f8f1
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-19 01:11:01 +0300

    add greeting + choosing if the user is an admin or a passenger to serve diff logics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        //greetings for logging in + asking if the user is a passenger or an admin
+        System.out.println("Welcome to Nasser Plane Ticketing System.\n Are you Admin/Passenger? ");
+        String you = sc.nextLine();
+        if (you.equalsIgnoreCase("Admin")) {
+            //he can add flights
+        } else if (you.equalsIgnoreCase("Passenger")) {
+            //he can book flights
+        }
     }
 }
