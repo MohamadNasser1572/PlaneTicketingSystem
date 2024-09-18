156904d0748e38ee917be348eb0e93d5da7180b0
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-19 01:25:41 +0300

    implement logic for flight model class

--- src/model/Flight.java
+++ src/model/Flight.java
@@ -1,4 +1,100 @@
 package model;
 
 public class Flight {
+    private int flightID;
+    private String departureCity;
+    private String arrivalCity;
+    private String departureTime;
+    private String arrivalTime;
+    private String departureDate;
+    private int availableSeats;
+    private int totalSeats;
+    private double price;
+
+    public Flight() {
+    }
+
+    public Flight(int flightID, String departureCity, String arrivalCity, String departureTime, String arrivalTime, String departureDate, int availableSeats, int totalSeats, double price) {
+        this.flightID = flightID;
+        this.departureCity = departureCity;
+        this.arrivalCity = arrivalCity;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.departureDate = departureDate;
+        this.availableSeats = availableSeats;
+        this.totalSeats = totalSeats;
+        this.price = price;
+    }
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public void setFlightID(int flightID) {
+        this.flightID = flightID;
+    }
+
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    public void setDepartureCity(String departureCity) {
+        this.departureCity = departureCity;
+    }
+
+    public String getArrivalCity() {
+        return arrivalCity;
+    }
+
+    public void setArrivalCity(String arrivalCity) {
+        this.arrivalCity = arrivalCity;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
