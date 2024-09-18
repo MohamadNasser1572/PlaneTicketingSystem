1c37537e64061c5d455d2ac42873812d37c07322
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-19 01:27:13 +0300

    add ticket model class logic

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -1,4 +1,12 @@
 package model;
 
+import java.util.Date;
+
 public class Ticket {
+    private int ticketId;
+    private Flight flight;
+    private Passenger passenger;
+    private int seatNumber;
+    private Date bookingDate;
+
 }
