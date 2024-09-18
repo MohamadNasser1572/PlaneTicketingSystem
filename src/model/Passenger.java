142d9c5229b8f9c8e5fcf3c51dc643da8624ef89
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-19 00:49:26 +0300

    ignore mysql connection class

---
+++ src/model/Passenger.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Passenger {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String email;
+
+    public Passenger() {
+    }
+
+    public Passenger(int id, String firstName, String lastName, int phoneNumber, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
