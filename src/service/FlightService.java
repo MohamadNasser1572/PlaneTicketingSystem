b9d0abf91fd1b5e400854ee745ea197d44bba84f
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-09-19 14:09:23 +0300

    add logic for flight service

--- src/service/FlightService.java
+++ src/service/FlightService.java
@@ -1,2 +1,27 @@
-package service;public class FlightService {
+package service;
+
+import model.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightService {
+    private List<Flight> flightDatabase = new ArrayList<>();
+
+    public void addFlight(Flight flight) {
+        flightDatabase.add(flight);
+    }
+
+    public Flight findFlightById(int id) {
+        return flightDatabase.stream().filter(flight -> flight.getId() == id).findFirst().orElse(null);
+    }
+
+    public List<Flight> findAllFlights() {
+        return new ArrayList<>(flightDatabase);
+    }
+
+    public void removeFlight(int id) {
+        flightDatabase.removeIf(flight -> flight.getId() == id);
+    }
 }
+
